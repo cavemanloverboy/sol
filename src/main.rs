b2298aef5a722b631c0d57e3210b47e2439c9c14
Eliascm17 <Elias.cmoreno17@gmail.com>
2024-01-08 10:12:10 -0600

    update rpc_url arg

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 
 use clap::Parser;
 
+use solana_cli_config::{Config, CONFIG_FILE};
 use solana_sdk::{pubkey::Pubkey, signature::Signature};
 
 mod account;
@@ -16,14 +17,18 @@ pub struct ExplorerCli {
     #[command(subcommand)]
     command: Command,
 
-    /// The url/endpoint to use for any rpc requests.
-    #[arg(
-        long,
-        short = 'u',
-        default_value = "http://api.mainnet-beta.solana.com",
-        global = true
-    )]
-    rpc_url: String,
+    /// Optionally include your RPC endpoint. Use "local", "dev", "main" for default endpoints. Defaults to your Solana CLI config file.
+    #[clap(global = true, short = 'u', long)]
+    rpc_url: Option<String>,
+}
+
+pub fn get_network(network_str: &str) -> &str {
+    match network_str {
+        "devnet" | "dev" | "d" => "https://api.devnet.solana.com",
+        "mainnet" | "main" | "m" | "mainnet-beta" => "https://api.mainnet-beta.solana.com",
+        "localnet" | "localhost" | "l" | "local" => "http://localhost:8899",
+        _ => network_str,
+    }
 }
 
 #[derive(Debug, Parser, Clone)]
@@ -52,9 +57,19 @@ pub struct Account {
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let args = ExplorerCli::parse();
+    let config = match CONFIG_FILE.as_ref() {
+        Some(config_file) => Config::load(config_file).unwrap_or_else(|_| {
+            println!("Failed to load config file: {}", config_file);
+            Config::default()
+        }),
+        None => Config::default(),
+    };
+    let network_url = &get_network(&args.rpc_url.unwrap_or(config.json_rpc_url)).to_string();
 
     match args.command {
-        Command::Transaction(transaction) => transaction::handler(args.rpc_url, transaction).await,
-        Command::Account(account) => account::handler(args.rpc_url, account).await,
+        Command::Transaction(transaction) => {
+            transaction::handler(network_url.to_string(), transaction).await
+        }
+        Command::Account(account) => account::handler(network_url.to_string(), account).await,
     }
 }
